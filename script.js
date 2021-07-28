568b75ec78cc44c96f66b0834ca7edd7c73a9c68
Joseph Chairez <joseph_chairez@yahoo.com>
2021-07-28 15:51:17 -0700

    initiat commit

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("loading page...");
+
+var cookieSpan = document.querySelector(".btn");
+
+function accept() {
+  cookieSpan.remove();
+}
+
+function emptyCart() {
+  alert("Your Cart is empy!");
+}
